644e0d96920d14803ba82ab91efe8e0a29599a5d
Chris Fried <fried.chris@gmail.com>
2021-11-13 14:18:48 -0600

    initial commit

---
+++ apps/party/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'chrisfried-destiny-projects-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'party';
+}
